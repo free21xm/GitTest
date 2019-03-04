1c4222bba104541917819c5e935f66e1def9acca
free21xm <1162640546@qq.com>
2019-03-04 19:02:45 +0800

    Initial commit

---
+++ src/com/edu/demo/Hello.java
@@ -0,0 +1,12 @@
+package com.edu.demo;
+
+/**
+ * @author chenbo
+ * @create 2019-03-04 18:59
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
