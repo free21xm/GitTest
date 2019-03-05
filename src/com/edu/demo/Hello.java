f7e178307635a66dce3a963b5cf676e876a9a118
free21xm <1162640546@qq.com>
2019-03-05 10:42:07 +0800

    更新Hello.java添加sout（nihao）

--- src/com/edu/demo/Hello.java
+++ src/com/edu/demo/Hello.java
@@ -9,5 +9,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("你好");
         System.out.println("你好呀");
+        System.out.println("nihao");
     }
 }
